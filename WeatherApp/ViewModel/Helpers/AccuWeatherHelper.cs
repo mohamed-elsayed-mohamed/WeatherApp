81c31945e7074792cb130f65a1db1c7c86e7252e
Mohamed Elsayed <mohamed_elsayed.1995@yahoo.com>
2021-10-12 03:09:24 +0200

    Init AccuWeatherHelper

---
+++ WeatherApp/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_URL = "currentconditions/v1/{0}?apikey={1}";
+        public const string API_KEY = "Z6rUI9LZ8c261sKJ1djCTERC8KAvkPoe";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditions(string citykey)
+        {
+            CurrentConditions currentConditions = new CurrentConditions();
+
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_URL, citykey, API_KEY);
+
+            using(HttpClient client = new HttpClient())
+            {
+                var respose = await client.GetAsync(url);
+                string json = await respose.Content.ReadAsStringAsync();
+
+                currentConditions = JsonConvert.DeserializeObject<List<CurrentConditions>>(json).FirstOrDefault();
+            }
+
+            return currentConditions;
+        }
+    }
+}
